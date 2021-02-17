0316be8c1c4b657960777b8b4c2cfd7a31006ffd
StopGuard <stopguard@bk.ru>
2021-02-18 01:59:45 +0500

    Lessons tutorials added

---
+++ Lesson1.py
@@ -0,0 +1,21 @@
+"""
+id(a) - ссылка на область в памяти где хранится объект
+
+
+Приоритет выполнения операций в Python
+от самого низкого к самому высокому:
+    =
+    if – else
+    or
+    and
+    not x
+    in, not in, is, is not, <, <=, >, >=, !=, ==
+    +, -
+    *, /, //, %
+    +x, -x
+    **
+    x[index], x[index:index], x(arguments...), x.attribute
+    (expressions...),
+    [expressions...], {key: value...}, {expressions...}
+    ( )
+"""
